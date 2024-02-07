c43ffd256c0b9473dfd8f1c6fa0a50ddcf255a52
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2024-02-07 17:08:16 +0200

    basic temp

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/react-in-jsx-scope */
+import ReactDOM from 'react-dom';
+import { Root } from './Root';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root') as HTMLElement,
+);
