875d17bce40ff2e034b9c5bb313377019aafcf67
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2024-02-09 23:35:14 +0200

    added server

--- src/helpers/fetchMain.ts
+++ src/helpers/fetchMain.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const BASE_URL = 'http://192.168.0.154:3005';
+const BASE_URL = 'https://task-management-server-0c0r.onrender.com';
 
 function wait(delay: number) {
   return new Promise(resolve => {
