d8a53a4e682f8c31508756e490957c3028c07fe8
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2024-03-01 11:57:17 +0200

    new link

--- src/helpers/fetchMain.ts
+++ src/helpers/fetchMain.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const BASE_URL = 'https://task-management-server-0c0r.onrender.com';
+const BASE_URL = 'https://task-management-server-0sta.onrender.com';
 
 function wait(delay: number) {
   return new Promise(resolve => {
